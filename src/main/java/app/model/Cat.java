775cf8787ace44dc56efe79de05b935153f4ad3d
Zkoiran <122123yc>
2020-07-12 22:55:10 +0700

    Done by Ilya Krasavin

--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -1,5 +1,6 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,7 +1,12 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dog extends Animal {
+    @Autowired
+    private Animal animal;
+
 }
